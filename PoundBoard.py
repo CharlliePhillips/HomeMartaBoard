ab2a5893967be79711bbde0b8f51d143d1f08171
CharliePhillips <cp865617@gmail.com>
2022-10-12 16:31:38 -0400

    should have minor issues fixed

--- PoundBoard.py
+++ PoundBoard.py
@@ -33,55 +33,45 @@ canvas.configure(bg="WHITE")
        
 def update():
     print("Updating...")
+    
+    #should clear the screen on each update to ensure that one train doesn't get printed twice with different times
+    blanks = []
+    for i in range(0,16):
+        blanks.append(tkinter.Label(root,text="",padx=0,pady=0,bg="WHITE",anchor='w'))
+        for j in range(0,4):
+            blanks[i].grid(columnspan=1,column=i,row=int(j/4),sticky='NSEW')
+            
     #pull current trains from the marta api at five points
     trains = MARTA.get_trains(MARTA, station="Five Points Station")
 
     #start sorting array up by time until arrival
-    for i in range(1,len(trains)):
-        lastTrain = trains[i - 1]
-        if(trains[i].waiting_time != "Arriving"):
-            currTime = int(trains[i].waiting_time.split(" ")[0])
-        else:
-            currTime = -1
-        if(lastTrain.waiting_time != "Arriving"):
-            lastTime = int(lastTrain.waiting_time.split(" ")[0])
-        else:
-            lastTime = -1
+    for j in reversed(range(1,len(trains))):
+        for i in range(j,len(trains)):
+            lastTrain = trains[i - 1]
+            if(trains[i].waiting_time != "Arriving"):
+                currTime = int(trains[i].waiting_time.split(" ")[0])
+            else:
+                currTime = -1
+            if(lastTrain.waiting_time != "Arriving"):
+                lastTime = int(lastTrain.waiting_time.split(" ")[0])
+            else:
+                lastTime = -1
         
-        if(currTime < lastTime):
-            trains[i-1] = trains[i]
-            trains[i] = lastTrain
-    #sort array backwards by time until arrival
-    for i in reversed(range(0,len(trains)-1)):
-        lastTrain = trains[i + 1]
-        if(trains[i].waiting_time != "Arriving"):
-            currTime = int(trains[i].waiting_time.split(" ")[0])
-        else:
-            currTime = -1
-        if(lastTrain.waiting_time != "Arriving"):
-            lastTime = int(lastTrain.waiting_time.split(" ")[0])
-        else:
-            lastTime = -1
-        
-        if(currTime > lastTime):
-            trains[i+1] = trains[i]
-            trains[i] = lastTrain
-        
+            if(currTime < lastTime):
+                trains[i-1] = trains[i]
+                trains[i] = lastTrain
     lines = []
     times = []
     for i in range(0,len(trains)):
         time = timeText(trains[i].waiting_time)
-        times.append(tkinter.Label(root,text=time,padx=20,pady=0,font=("Arial",30),bg="WHITE",anchor='w'))
-    for i in range(0,len(trains)):
-        line = trains[i].direction
-        lines.append(tkinter.Label(root,text=line,bg=trains[i].line,fg="WHITE",padx=0,pady=0,font=("Arial Bold",45)))
-
-    for i in range(0,len(times)):
+        times.append(tkinter.Label(root,text=time,padx=10,pady=0,font=("Arial",35),bg="WHITE",anchor='w'))
         if(i < 4):
             times[i].grid(columnspan=1,column=1,row=i,sticky='NSEW')
         if(i > 3):
             times[i].grid(columnspan=1,column=3,row=i-4,sticky='NESW')
-    for i in range(0,len(lines)):
+    for i in range(0,len(trains)):
+        line = trains[i].direction
+        lines.append(tkinter.Label(root,text=line,bg=trains[i].line,fg="WHITE",padx=0,pady=0,font=("Arial Bold",60)))
         if(i < 4):
             lines[i].grid(columnspan=1,column=0,row=i,sticky='NESW')
         if(i > 3):
